61f139e963ce41e609d9b6a458e57f570b6f4c30
sscovil <sscovil@gmail.com>
2024-07-05 14:37:41 -0400

    add ability to rename GSS files

--- addons/godot_style_sheets/gss_code_edit.gd
+++ addons/godot_style_sheets/gss_code_edit.gd
@@ -2,11 +2,11 @@
 class_name GSSCodeEdit
 extends CodeEdit
 
-var current_indent: int = 0
-var current_line: String = ""
-var current_theme_type: String = ""
-var current_style: String = ""
-var current_stylebox: String = ""
+var _current_indent: int = 0
+var _current_line: String = ""
+var _current_theme_type: String = ""
+var _current_style: String = ""
+var _current_stylebox: String = ""
 
 var file_system: EditorFileSystem = EditorInterface.get_resource_filesystem()
 
@@ -18,6 +18,14 @@ func _ready() -> void:
 	set_syntax_highlighter(GSSSyntaxHighlighter.new())
 	
 	text_changed.connect(_on_text_changed)
+
+
+func clear_state() -> void:
+	_current_indent = 0
+	_current_line = ""
+	_current_theme_type = ""
+	_current_style = ""
+	_current_stylebox = ""
 
 
 func _add_code_completion_options(options: Array, kind: int, color: Color, location: int) -> void:
@@ -56,11 +64,11 @@ func _on_resources_reimported(resources: PackedStringArray) -> void:
 func _on_text_changed() -> void:
 	_update_current_state()
 	
-	var is_before_colon: bool = current_line.find(":") == -1
+	var is_before_colon: bool = _current_line.find(":") == -1
 	
 	if is_before_colon:
 		# Find the proper code completion options for the current indentation level.
-		match current_indent:
+		match _current_indent:
 			0: _update_theme_type_code_completion_options()
 			1: _update_theme_style_code_completion_options()
 			2: _update_stylebox_code_completion_options()
@@ -73,31 +81,30 @@ func _update_current_state() -> void:
 	var clean_text: String = GSS.strip_comments(text)
 	var lines: PackedStringArray = clean_text.split("\n")
 	
-	current_line = lines[cursor_line]
-	current_indent = GSS.get_indentation_level(current_line)
-	current_theme_type = ""
-	current_style = ""
-	current_stylebox = ""
+	clear_state()
+	
+	_current_line = lines[cursor_line]
+	_current_indent = GSS.get_indentation_level(_current_line)
 	
 	# Step backwards from current line to determine current theme type & style being edited.
 	for i in range(cursor_line, -1, -1):
 		var line = lines[i].strip_edges()
 		
 		if line.ends_with(":"):
-			match current_indent:
-				0: current_theme_type = line.trim_suffix(":")
-				1: current_style = line.trim_suffix(":")
+			match _current_indent:
+				0: _current_theme_type = line.trim_suffix(":")
+				1: _current_style = line.trim_suffix(":")
 		elif line.begins_with("stylebox:"):
-			current_stylebox = line.get_slice(":", 1).strip_edges()
+			_current_stylebox = line.get_slice(":", 1).strip_edges()
 		
 		# When we find the current theme type, ensure current style is set and exit the `for` loop.
-		if current_theme_type:
-			if !current_style:
-				current_style = GSS.DEFAULT_STYLE
+		if _current_theme_type:
+			if !_current_style:
+				_current_style = GSS.DEFAULT_STYLE
 			break
 	
 	# Step forward from the current line to determine the current flavor of StyleBox, if defined.
-	if 2 == current_indent and !current_stylebox:
+	if 2 == _current_indent and !_current_stylebox:
 		for i in range(cursor_line, 1, 1):
 			var line = lines[i].strip_edges()
 			
@@ -105,10 +112,10 @@ func _update_current_state() -> void:
 				break  # Stop when a property is defined at a lower indentation level.
 			
 			if line.begins_with("stylebox:"):
-				current_stylebox = line.get_slice(":", 1).strip_edges()
+				_current_stylebox = line.get_slice(":", 1).strip_edges()
 	
-	if !current_stylebox:
-		current_stylebox = GSS.DEFAULT_STYLEBOX
+	if !_current_stylebox:
+		_current_stylebox = GSS.DEFAULT_STYLEBOX
 
 
 func _update_stylebox_code_completion_options() -> void:
@@ -130,7 +137,7 @@ func _update_theme_type_code_completion_options() -> void:
 	)
 
 func _update_theme_style_code_completion_options() -> void:
-	var style_options = GSS.get_theme_properties(current_style)
+	var style_options = GSS.get_theme_properties(_current_style)
 	var stylebox_options = GSS.get_stylebox_properties(GSS.DEFAULT_STYLEBOX)
 	
 	set_code_completion_prefixes(style_options + stylebox_options)
--- addons/godot_style_sheets/gss_editor.gd
+++ addons/godot_style_sheets/gss_editor.gd
@@ -2,12 +2,19 @@
 class_name GSSEditor
 extends VBoxContainer
 
-const AUTO_SAVE_DELAY := 2.0  # Seconds to wait before saving, after a change is made.
+## Seconds to wait before auto-saving the current file, if a change has been made.
+const AUTO_SAVE_DELAY: float = 2.0
+
+## Size of the file dialong window, when loading, saving, importing, or exporting a file.
 const FILE_DIALOG_POPUP_SIZE := Vector2(800, 600)
+
+## Seconds to wait before handling pending reimports.
+const REIMPORT_POLL_INTERVAL: float = 1.0
 
 var all_files: Dictionary = {}
 var current_file: String = ""
-var file_dialog: FileDialog
+var tres_file_dialog: FileDialog
+var gss_file_dialog: FileDialog
 var file_hashes: Dictionary = {}
 var file_system: EditorFileSystem = EditorInterface.get_resource_filesystem()
 var is_reimporting: bool = false
@@ -19,58 +26,95 @@ var save_timer: Timer
 @onready var file_menu: PopupMenu = $SplitContainer/FileListContainer/FileListMenuBar/File
 @onready var file_list: ItemList = $SplitContainer/FileListContainer/ScrollContainer/FileList
 @onready var file_editor: CodeEdit = $SplitContainer/FileEditorContainer/FileEditor
+@onready var file_rename: LineEdit = $SplitContainer/FileListContainer/ScrollContainer/FileList/RenameFile
 
 
 func _ready() -> void:
-	file_menu.id_pressed.connect(_on_file_menu_id_pressed)
-
-	file_dialog = FileDialog.new()
-	file_dialog.access = FileDialog.ACCESS_RESOURCES
-	file_dialog.file_mode = FileDialog.FILE_MODE_OPEN_FILE
-	file_dialog.add_filter("*.gss ; GSS Files")
-	add_child(file_dialog)
-
-	file_dialog.file_selected.connect(_on_file_selected)
+	# Add FileDialog window for GSS files.
+	gss_file_dialog = _new_file_dialog_node("*.gss ; GSS Files")
+	gss_file_dialog.file_selected.connect(_on_gss_file_selected)
+	add_child(gss_file_dialog)
 	
-	file_editor.text_changed.connect(_on_file_editor_text_changed)
+	# Add FileDialog window for Theme files.
+	tres_file_dialog = _new_file_dialog_node("*.tres ; Theme Files")
+	tres_file_dialog.file_selected.connect(_on_tres_file_selected)
+	add_child(tres_file_dialog)
 	
-	file_list.item_selected.connect(_on_file_list_item_selected)
-	
+	# Add timer for auto-save.
 	save_timer = Timer.new()
 	save_timer.one_shot = true
 	save_timer.timeout.connect(_save_current_file)
 	add_child(save_timer)
 	
+	# Add timer for handling pending GSS file reimports.
 	reimport_timer = Timer.new()
-	reimport_timer.wait_time = 1.0  # Check every second
+	reimport_timer.autostart = true
 	reimport_timer.one_shot = false
 	reimport_timer.timeout.connect(_process_pending_reimports)
+	reimport_timer.wait_time = REIMPORT_POLL_INTERVAL
 	add_child(reimport_timer)
-	reimport_timer.start()
 	
+	# Handle file editor text changes.
+	file_editor.text_changed.connect(_on_file_editor_text_changed)
+	
+	# Handle clicks in the GSS file list sidebar.
+	file_list.item_selected.connect(_on_file_list_item_selected)
+	
+	# Handle clicks in the `File` dropdown menu.
+	file_menu.id_pressed.connect(_on_file_menu_id_pressed)
+	
+	# Set up inline file renaming in the GSS file list.
+	file_list.set_allow_reselect(true)  # Needed to detect click on currently selected file.
+	file_rename.set_visible(false)
+	file_rename.text_change_rejected.connect(_on_file_rename_text_change_rejected)
+	file_rename.text_submitted.connect(_on_file_rename_text_submitted)
+	file_rename.focus_exited.connect(_on_file_rename_focus_lost)
+	
+	# Find all GSS files in the project and add them to the file list sidebar.
 	_populate_file_list()
 
 
+## Clears the contents and current state of the GSS file editor.
+func _clear_editor_state() -> void:
+	current_file = ""
+	file_editor.clear_state()
+	file_editor.set_text("")
+
+
+## Exports a GSS file as a Theme file.
+func _export() -> void:
+	tres_file_dialog.file_mode = FileDialog.FILE_MODE_SAVE_FILE
+	tres_file_dialog.popup_centered(FILE_DIALOG_POPUP_SIZE)
+
+
+## Loads a GSS file in the editor.
 func _load_file() -> void:
-	file_dialog.file_mode = FileDialog.FILE_MODE_OPEN_FILE
-	file_dialog.popup_centered(FILE_DIALOG_POPUP_SIZE)
+	gss_file_dialog.file_mode = FileDialog.FILE_MODE_OPEN_FILE
+	gss_file_dialog.popup_centered(FILE_DIALOG_POPUP_SIZE)
 
 
+## Creates a new GSS file and open it in the editor.
 func _new_file() -> void:
+	var timestamp: int = floor(Time.get_unix_time_from_system())
+	var file_name: String = "new_%d.gss" % timestamp
 	current_file = ""
 	file_editor.text = ""
-	all_files["Untitled"] = ""
+	all_files[file_name] = ""
 	_update_file_list()
 
 
-func _on_file_menu_id_pressed(id: int) -> void:
-	match id:
-		0: _new_file()
-		1: _load_file()
-		2: _save_file()
-		3: _save_file_as()
+## Instantiates a new FileDialog node with the given filter.
+func _new_file_dialog_node(filter: String) -> FileDialog:
+	var file_dialog := FileDialog.new()
+	
+	file_dialog.access = FileDialog.ACCESS_RESOURCES
+	file_dialog.file_mode = FileDialog.FILE_MODE_OPEN_FILE
+	file_dialog.add_filter(filter)
+	
+	return file_dialog
 
 
+## Handles file editor text changes.
 func _on_file_editor_text_changed() -> void:
 	if !current_file:
 		return
@@ -79,27 +123,114 @@ func _on_file_editor_text_changed() -> void:
 	save_timer.start(AUTO_SAVE_DELAY)
 
 
+## Handles clicks in the GSS file list sidebar.
 func _on_file_list_item_selected(index: int) -> void:
 	var path: String = file_list.get_item_metadata(index)
 	
-	if path in all_files:
-		_save_current_file()  # Save current file before switching.
-		current_file = path
-		file_editor.set_text(all_files[path])
-		file_editor.set_caret_line(0)  # Set cursor to start of file.
-
-
-func _on_file_selected(path: String) -> void:
-	if file_dialog.file_mode == FileDialog.FILE_MODE_OPEN_FILE:
+	if not path in all_files:
 		_read_file(path)
-	elif file_dialog.file_mode == FileDialog.FILE_MODE_SAVE_FILE:
+	
+	if current_file == path:
+		_rename_file(path)
+		return
+	
+	_save_current_file()  # Save current file before switching.
+	current_file = path
+	file_editor.set_text(all_files[path])
+	file_editor.set_caret_line(0)  # Set cursor to start of file.
+
+
+## Handles clicks in the `File` dropdown menu.
+func _on_file_menu_id_pressed(id: int) -> void:
+	match id:
+		0: _new_file()
+		1: _load_file()
+		2: _save_file()
+		3: _save_file_as()
+		4: _export()
+
+
+func _on_file_rename_text_change_rejected(rejected_substring: String) -> void:
+	_on_file_renamed(current_file.get_file())
+
+
+func _on_file_rename_text_submitted(new_text: String) -> void:
+	_on_file_renamed(new_text)
+
+
+func _on_file_rename_focus_lost() -> void:
+	if file_rename.is_visible():
+		_on_file_renamed(file_rename.get_text())
+
+
+func _on_file_renamed(new_name: String) -> void:
+	file_rename.set_text("")
+	file_rename.set_visible(false)
+	
+	var old_name: String = current_file.get_file()
+	
+	if new_name == old_name:
+		return
+	
+	var base_dir: String = current_file.get_base_dir()
+	var new_path: String = base_dir.path_join(new_name)
+	var old_path: String = current_file
+	
+	if FileAccess.file_exists(new_path):
+		push_warning("[GSS] Unable to rename file because file already exists: %s" % new_path)
+		return
+	
+	var dir: DirAccess = DirAccess.open(base_dir)
+	
+	# Remove the old import file if it exists.
+	var old_import: String = "%s.import" % old_name
+	if dir.file_exists(old_import):
+		dir.remove(old_import)
+	
+	# Rename the file.
+	dir.rename(old_path, new_path)
+	
+	# Update current state.
+	all_files[new_path] = all_files[old_path]
+	all_files.erase(old_path)
+	file_hashes[new_path] = file_hashes[old_path]
+	file_hashes.erase(old_path)
+	current_file = new_path
+	
+	# Tell Godot's internal file system object about the changes (required for reimport).
+	file_system.update_file(old_path)
+	file_system.update_file(new_path)
+	
+	# Queue file for reimport with new name.
+	if not new_path in pending_reimports:
+		pending_reimports.append(new_path)
+	
+	_update_file_list(file_list.get_selected_items()[0])
+
+
+## Handles GSS file dialog when file is selected.
+func _on_gss_file_selected(path: String) -> void:
+	if gss_file_dialog.file_mode == FileDialog.FILE_MODE_OPEN_FILE:
+		_read_file(path)
+	elif gss_file_dialog.file_mode == FileDialog.FILE_MODE_SAVE_FILE:
 		_write_file(path)
 
 
+## Handles Theme file dialog when file is selected.
+func _on_tres_file_selected(path: String) -> void:
+	if gss_file_dialog.file_mode == FileDialog.FILE_MODE_OPEN_FILE:
+		push_warning("Importing theme files as GSS is not yet possible.")
+		pass
+	elif gss_file_dialog.file_mode == FileDialog.FILE_MODE_SAVE_FILE:
+		GSS.file_to_tres(current_file)
+
+
+## Finds all GSS files in the project and adds them to the file list sidebar.
 func _populate_file_list() -> void:
-	_scan_directory("res://")
+	_scan_directory("res://", "gss")
 
 
+## Reimports an GSS files that were queued to be reimported.
 func _process_pending_reimports() -> void:
 	if is_reimporting or pending_reimports.is_empty():
 		return
@@ -121,6 +252,8 @@ func _process_pending_reimports() -> void:
 	is_reimporting = false
 
 
+## Reads the file at the given path and stores its contents as `current_file`. Also adds it to the
+## `all_files` dictionary, and adds the file hash to the `file_hashes` dictionary.
 func _read_file(path: String) -> void:
 	var file = FileAccess.open(path, FileAccess.READ)
 	
@@ -131,8 +264,31 @@ func _read_file(path: String) -> void:
 		all_files[path] = file_editor.text
 		file_hashes[path] = all_files[path].hash()
 		_update_file_list()
+	else:
+		# File was not readable, so remove it from the list.
+		push_warning("[GSS] Unable to open file: %s" % path)
+		all_files.erase(path)
+		file_hashes.erase(path)
+	
+	_update_file_list()
 
 
+## Enables the user to rename a given file in place, in the file list.
+func _rename_file(path: String) -> void:
+	var index: int = file_list.get_selected_items()[0]
+	var file_name: String = file_list.get_item_text(index)
+	var file_list_global_pos: Vector2 = file_list.get_global_position()
+	var file_list_item_pos: Vector2 = file_list.get_item_rect(index, true).position
+	
+	file_rename.set_text(file_name)
+	file_rename.set_visible(true)
+	file_rename.set_global_position(file_list_global_pos + file_list_item_pos)
+	file_rename.set_size(file_list.get_item_rect(index).size)
+	file_rename.grab_focus()
+	file_rename.select(0, file_name.length() - 4)  # Select file name, not file extension.
+
+
+## Saves the current file, if it has been changed (based on file hash).
 func _save_current_file() -> void:
 	if !current_file:
 		return
@@ -142,6 +298,7 @@ func _save_current_file() -> void:
 		_write_file(current_file)
 
 
+## Saves the current file if set; otherwise, opens the 'Save File As...' dialog.
 func _save_file() -> void:
 	if current_file.is_empty():
 		_save_file_as()
@@ -149,12 +306,14 @@ func _save_file() -> void:
 		_write_file(current_file)
 
 
+## Opens the 'Save File As...' dialog for the code that is currently in the GSS editor.
 func _save_file_as() -> void:
-	file_dialog.file_mode = FileDialog.FILE_MODE_SAVE_FILE
-	file_dialog.popup_centered(FILE_DIALOG_POPUP_SIZE)
+	gss_file_dialog.file_mode = FileDialog.FILE_MODE_SAVE_FILE
+	gss_file_dialog.popup_centered(FILE_DIALOG_POPUP_SIZE)
 
 
-func _scan_directory(path: String) -> void:
+## Scans the given path recursively, calling `_read_file()` for each file with the given extension.
+func _scan_directory(path: String, file_extension: String) -> void:
 	var dir = DirAccess.open(path)
 	
 	if dir:
@@ -169,8 +328,8 @@ func _scan_directory(path: String) -> void:
 			var full_path = path.path_join(file_name)
 			
 			if dir.current_is_dir():
-				_scan_directory(full_path)  # Recursive call for subdirectories.
-			elif file_name.get_extension() == "gss":
+				_scan_directory(full_path, file_extension)  # Recursive call for subdirectories.
+			elif file_name.get_extension() == file_extension:
 				_read_file(full_path)
 				
 			file_name = dir.get_next()
@@ -178,7 +337,8 @@ func _scan_directory(path: String) -> void:
 		dir.list_dir_end()
 
 
-func _update_file_list() -> void:
+## Updates the list of GSS file names in the sidebar, with the full file path as tooltip text.
+func _update_file_list(select_index: int = 0) -> void:
 	file_list.clear()
 	
 	var sorted_paths: Array = all_files.keys()
@@ -193,14 +353,15 @@ func _update_file_list() -> void:
 		file_list.set_item_tooltip(index, path)  # Set full path as tooltip text.
 	
 	# Set focus to the first item if the list is not empty.
-	if file_list.get_item_count() > 0:
-		file_list.select(0)
-		_on_file_list_item_selected(0)
+	if file_list.get_item_count() > select_index:
+		file_list.select(select_index)
+		_on_file_list_item_selected(select_index)
 	
 	# Ensure the FileList is visible in the editor.
 	file_list.ensure_current_is_visible()
 
 
+## Saves the contents of the GSS file editor to a file at the given path.
 func _write_file(path: String) -> void:
 	var file = FileAccess.open(path, FileAccess.WRITE)
 	
